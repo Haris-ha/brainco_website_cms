6b8e45b9c3f522b18c90b9a95a13ed66dcd82327
LishiHa <lishi_ha@163.com>
2025-10-26 08:45:38 +0800

    feat: add i18n

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = () => ({
+    i18n: {
+        enabled: true,
+        config: {
+          defaultLocale: 'en',
+          locales: ['en', 'zh-Hans', 'zh-Hant'], // 英文、简体中文、繁体中文
+        },
+      },
+});
